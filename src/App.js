c79b92b4a98869421c4a0f188e5976ab5d605394
fuzzy95 <asif.eshaq@northsouth.edu>
2020-10-26 14:46:04 +0600

    initiated tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <h1 className="text-4xl font-bold text-blue-300">
+      It works
+    </h1>
   )
 }
 
